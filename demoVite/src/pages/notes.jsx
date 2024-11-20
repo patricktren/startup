13ef5d2cdc60d0517348d3f54a6213b73ffd4b91
patricktren <patricktrenwarren@gmail.com>
2024-11-19 22:04:35 -0700

    Added functionality to add folders and name them.

--- demoVite/src/pages/notes.jsx
+++ demoVite/src/pages/notes.jsx
@@ -9,15 +9,74 @@ import folder_image from '../images/folder.png';
 
 
 export function Notes() {
+    const [folders, setFolders] = useState([ {} ]);
+
+    const addFolder = () => {
+        setFolders((folders) => [...folders, { }]);
+    }
+
+    const [currFolder, selectFolder] = useState(folders[0]);
+
+    const setCurrFolder = (event) => {
+        console.log(currFolder.pages);
+    }
+
+
+    return (
+        <main className="main-notes">
+            <section className="section-notes">
+                <h4 style={{ textAlign: "center", marginTop: "5px" }}>Folders</h4>
+                <ul className="ul-notes">
+                    {folders.map((folder, index) => {
+                        return (<Folder key={index} initFolderName={folder.initFolderName} />);
+                    })}
+                    <li className='btn btn-green' onClick={addFolder}>Add folder</li>
+                </ul>
+            </section>
+            <section className="section-notes">
+                <h4 style={{ textAlign: "center", marginTop: "5px" }}>Pages</h4>
+                <ul className="ul-notes">
+                    {currFolder.pages && currFolder.pages.map((page, index) => {
+                        return (<li key={index}>{page.name}</li>);
+                    })}
+                    <li className='btn btn-green' onClick={setCurrFolder}>ree</li>
+                </ul>
+            </section>
+            <Page name='ree' />
+        </main>
+    )
+}
+
+function Folder() {
+    const [folderName, setName] = useState('myFolder');
+    
+    const [readOnly, setReadOnly] = useState(false);
+    const rename = (event) => {
+        setName(event.target.value);
+    }
+    const finishRename = (event) => {
+        if (event.key==='Enter') {
+            setReadOnly(true);
+        }
+    }
+
+    const [pages, setPages] = useState([]);
+
+    return (
+        <li className='li-notes'><input className='input-txt' onChange={rename} onKeyDown={finishRename} readOnly={readOnly} value={folderName} /></li>
+    )
+}
+
+function Page({ initPageName }) {
+    const [pageName, setName] = useState(initPageName);
     const [nextNoteId, updateNoteId] = useState(1);
 
-    const [notes, setNotes] = useState([{id:0, x:50, y:50, text:''}]);
+    const [notes, setNotes] = useState([{ id: 0, x: 50, y: 50, text: '' }]);
     const addNote = (event) => {
         if (event.target.className == 'section-blackboard') {
             const x = event.nativeEvent.offsetX;
             const y = event.nativeEvent.offsetY;
-            console.log(x, y);
-            setNotes((prevNotes) => [...prevNotes, {id:nextNoteId, x:x, y:y, text:''}]);
+            setNotes((prevNotes) => [...prevNotes, { id: nextNoteId, x: x, y: y, text: '' }]);
             // increment nextNoteId
             updateNoteId(nextNoteId + 1);
         }
@@ -26,55 +85,22 @@ export function Notes() {
     const deleteNote = (event) => {
         if (event.target.className == 'div-note-delete') {
             const idToDelete = event.target.getAttribute('id')
-            console.log(idToDelete);
             setNotes(notes.filter(note => note.id != idToDelete));
         }
     }
-
     return (
-        <main className="main-notes">
-            <section className="section-notes">
-                <img scr={folder_image} alt="Folder" width="100" />
-                <h4 style={{ textAlign: "center", marginTop: "5px" }}>Folders</h4>
-
-                <ul className="ul-notes">
-                    <li className="li-notes">Folder 1 (from database)</li>
-                    <li className="li-notes">Folder 2</li>
-                    <li className="li-notes">Folder 3 (shared via Websockets)</li>
-                </ul>
-            </section>
-            <section className="section-notes">
-                <h4 style={{ textAlign: "center", marginTop: "5px" }}>Pages</h4>
-                <ul className="ul-notes">
-                    <li className="li-notes">Page 1 (from database)</li>
-                    <li className="li-notes">Page 2 (from database)</li>
-                    <li className="li-notes">Page 3 (from database)</li>
-                </ul>
-            </section>
-
-            <section className="section-blackboard" onClick={(event) => { addNote(event); deleteNote(event) } }>
-                {notes.map((note, index) => {
-                    return (<Note key={index} id={note.id} x={note.x} y={note.y} text={note.text} />);
-                })}
-            </section>
-        </main>
+        <section className="section-blackboard" onClick={(event) => { addNote(event); deleteNote(event) }}>
+            {notes.map((note, index) => {
+                return (<Note key={index} id={note.id} x={note.x} y={note.y} text={note.text} />);
+            })}
+        </section>
     )
 }
 
-function Page() {
-    return (
-        <section className="section-blackboard" onClick={(event) => { addNote(event); deleteNote(event) } }>
-                {notes.map((note, index) => {
-                    return (<Note key={index} id={note.id} x={note.x} y={note.y} text={note.text} />);
-                })}
-            </section>
-    )
-}
-
-function Note({id, x, y, text}) {
-    const [position, setPosition] = useState({x:x, y:y})
+function Note({ id, x, y, text }) {
+    const [position, setPosition] = useState({ x: x, y: y })
     const drag = (event, data) => {
-        setPosition({x: data.x, y: data.y});
+        setPosition({ x: data.x, y: data.y });
     }
     const [content, updateContent] = useState(text);
     const contentChange = (event) => {
